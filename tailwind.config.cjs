e6249bc6f27575bfd9e379ebcb754fa03d1d776b
Kevin Griffin <kevin@kevgriffin.com>
2023-09-04 13:09:02 -0400

    non-wp site

---
+++ tailwind.config.cjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Exo", defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        revconf: {
+          base: "#EA262D",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
